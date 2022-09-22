5b78acdaefa4268b000f4128d264e25ab0bf486b
takacsberni <takacsberni@gmail.com>
2022-09-22 16:10:33 +0200

    feat: app.js calculates the sum of the two inputs and sends it as a response

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const app = express();
 
+app.use(bodyparser.urlencoded({extended:true}));
+
 app.get('', (req, res)=> {
     res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/', (req, res)=>{
+    const nr1 = Number(req.body.num1);
+    const nr2 = Number(req.body.num2);
+    const sum = nr1 + nr2;
+    res.send('The sum of nr 1 and nr 2 is: ' + sum);
 })
 
 app.listen(3000, (res)=> {
